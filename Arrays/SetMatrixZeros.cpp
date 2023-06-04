9a3fa6c8fa866c7f6ca2e70dbdfb6cb1146765c4
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-06-04 17:24:40 +0530

    Create SetMatrixZeros.cpp

---
+++ Arrays/SetMatrixZeros.cpp
@@ -0,0 +1,22 @@
+void setZeros(vector<vector<int>> &matrix)
+{
+    int n = matrix.size(),m = matrix[0].size();
+    vector<int> rows(n,0);
+    vector<int> cols(m,0);
+    
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
+            if(matrix[i][j]==0){
+                rows[i] = 1;
+                cols[j] = 1;
+            }
+        }
+    }
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j <m;j++){
+            if(rows[i] or cols[j])
+                matrix[i][j] = 0;
+        }
+    }
+    
+}
