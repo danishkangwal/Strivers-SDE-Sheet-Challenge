01c17235cc629017cb4e13e22973cf1d078e1ecf
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-06-22 17:34:34 +0530

    Update searchIn2DMatrix.cpp

--- Arrays Part-II/searchIn2DMatrix.cpp
+++ Arrays Part-II/searchIn2DMatrix.cpp
@@ -23,3 +23,21 @@ bool searchMatrix(vector<vector<int>>& mat, int target) {
 
     return 0;
 }
+
+// sol 3
+bool searchMatrix(vector<vector<int>>& mat, int target) {
+    int low = 0,high = (mat.size()*mat[0].size())-1;
+    if(mat.empty())
+        return 0;
+    while(low <= high){
+        int mid = low + (high-low)/2;
+        int num = mat[mid / mat[0].size()][mid % mat[0].size()];
+        if(num == target)
+            return 1;
+        else if(num < target)
+            low = mid+1;
+        else
+            high = mid-1; 
+    }
+    return 0;
+}
