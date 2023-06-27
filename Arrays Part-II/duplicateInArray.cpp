acd0e5aa43d468d15643a7986ff055618377d51d
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-06-27 10:00:58 +0530

    Update duplicateInArray.cpp

--- Arrays Part-II/duplicateInArray.cpp
+++ Arrays Part-II/duplicateInArray.cpp
@@ -21,3 +21,17 @@ int findDuplicate(vector<int> &arr, int n){
 }
 
 // sol 3
+int findDuplicate(vector<int> &nums, int n){
+    int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        fast = nums[0];
+        while(fast != slow){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+}
