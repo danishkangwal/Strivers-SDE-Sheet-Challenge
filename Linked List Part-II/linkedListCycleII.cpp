c613e985179f91afe843fd92a1e2884351617d54
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-07-08 15:46:55 +0530

    Create linkedListCycleII.cpp

---
+++ Linked List Part-II/linkedListCycleII.cpp
@@ -0,0 +1,20 @@
+Node *firstNode(Node *head)
+    {
+        if(head==NULL || head->next==NULL)
+            return NULL;
+        
+        Node *slow = head,*fast = head;
+        while(fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                slow = head;
+                while(slow!=fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
