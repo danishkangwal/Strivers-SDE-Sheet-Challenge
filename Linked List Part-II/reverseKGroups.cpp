dc4899dcf633ebea37a672ad98238a97dee17127
Danish Kangwal <76109746+danishkangwal@users.noreply.github.com>
2023-07-17 12:39:58 +0530

    Update reverseKGroups.cpp

--- Linked List Part-II/reverseKGroups.cpp
+++ Linked List Part-II/reverseKGroups.cpp
@@ -51,3 +51,50 @@ ListNode* reverseKGroup(ListNode* head, int k) {
         
         return dummy->next;
     }
+
+// sol 3 (Code Studio)
+Node *getListAfterReverseOperation(Node *head, int n, int b[]){
+    if(head==NULL || head->next==NULL) 
+        return head;
+
+	
+
+	Node *dummy = new Node(0);
+	dummy->next = head;
+	Node *prev = dummy;
+	Node *curr=head,*nex;
+	int length = 0;
+    while(curr){
+        length++;
+        curr=curr->next;
+    }
+
+    
+    int i=0;
+
+	while(length>0 && i<n){
+		if(b[i]==0){
+			i++;
+			continue;
+		};
+		curr = prev->next;
+		nex = curr->next;
+        int k = min(length,b[i]);
+
+				for(int j=1;j<k;j++){
+				curr->next = nex->next;
+				nex->next = prev->next;
+				prev->next = nex;
+				nex = curr->next;
+			    }
+
+				prev = curr;
+				length-=b[i];
+				i++;
+		
+
+			
+		}
+
+		return dummy->next;
+	}
